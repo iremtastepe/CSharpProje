b8cc4384bd094177e4accaa4c885fc4aaf898c1a
iremtastepe <147312067+iremtastepe@users.noreply.github.com>
2025-03-19 13:13:54 +0300

    Add files via upload

---
+++ LibraryProject/LibraryProject/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace LibraryProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAd = new System.Windows.Forms.Label();
+            this.txtYazar = new System.Windows.Forms.Label();
+            this.ISBN = new System.Windows.Forms.Label();
+            this.textBoxAd = new System.Windows.Forms.TextBox();
+            this.textBoxYazar = new System.Windows.Forms.TextBox();
+            this.textBoxISBN = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ekle = new System.Windows.Forms.Button();
+            this.sil = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAd
+            // 
+            this.txtAd.AutoSize = true;
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(24, 46);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(97, 25);
+            this.txtAd.TabIndex = 0;
+            this.txtAd.Text = "Kitap Adi:";
+            // 
+            // txtYazar
+            // 
+            this.txtYazar.AutoSize = true;
+            this.txtYazar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYazar.Location = new System.Drawing.Point(18, 92);
+            this.txtYazar.Name = "txtYazar";
+            this.txtYazar.Size = new System.Drawing.Size(103, 25);
+            this.txtYazar.TabIndex = 1;
+            this.txtYazar.Text = "Yazar Adi:";
+            // 
+            // ISBN
+            // 
+            this.ISBN.AutoSize = true;
+            this.ISBN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ISBN.Location = new System.Drawing.Point(24, 130);
+            this.ISBN.Name = "ISBN";
+            this.ISBN.Size = new System.Drawing.Size(64, 25);
+            this.ISBN.TabIndex = 2;
+            this.ISBN.Text = "ISBN:";
+            // 
+            // textBoxAd
+            // 
+            this.textBoxAd.Location = new System.Drawing.Point(128, 48);
+            this.textBoxAd.Name = "textBoxAd";
+            this.textBoxAd.Size = new System.Drawing.Size(223, 22);
+            this.textBoxAd.TabIndex = 3;
+            // 
+            // textBoxYazar
+            // 
+            this.textBoxYazar.Location = new System.Drawing.Point(127, 95);
+            this.textBoxYazar.Name = "textBoxYazar";
+            this.textBoxYazar.Size = new System.Drawing.Size(223, 22);
+            this.textBoxYazar.TabIndex = 4;
+            // 
+            // textBoxISBN
+            // 
+            this.textBoxISBN.Location = new System.Drawing.Point(94, 134);
+            this.textBoxISBN.Name = "textBoxISBN";
+            this.textBoxISBN.Size = new System.Drawing.Size(223, 22);
+            this.textBoxISBN.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(607, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(693, 66);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ayşe İrem Taştepe                                                                " +
+    "                      \r\n222503025\r\n\r\n";
+            // 
+            // ekle
+            // 
+            this.ekle.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ekle.Location = new System.Drawing.Point(611, 46);
+            this.ekle.Name = "ekle";
+            this.ekle.Size = new System.Drawing.Size(113, 54);
+            this.ekle.TabIndex = 7;
+            this.ekle.Text = "KİTAP\r\n EKLE";
+            this.ekle.UseVisualStyleBackColor = true;
+            this.ekle.Click += new System.EventHandler(this.ekle_Click);
+            // 
+            // sil
+            // 
+            this.sil.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.sil.Location = new System.Drawing.Point(611, 106);
+            this.sil.Name = "sil";
+            this.sil.Size = new System.Drawing.Size(113, 39);
+            this.sil.TabIndex = 8;
+            this.sil.Text = "KİTAP SİL";
+            this.sil.UseVisualStyleBackColor = true;
+            this.sil.Click += new System.EventHandler(this.sil_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(9, 162);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(592, 276);
+            this.dataGridView1.TabIndex = 9;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.sil);
+            this.Controls.Add(this.ekle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxISBN);
+            this.Controls.Add(this.textBoxYazar);
+            this.Controls.Add(this.textBoxAd);
+            this.Controls.Add(this.ISBN);
+            this.Controls.Add(this.txtYazar);
+            this.Controls.Add(this.txtAd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAd;
+        private System.Windows.Forms.Label txtYazar;
+        private System.Windows.Forms.Label ISBN;
+        private System.Windows.Forms.TextBox textBoxAd;
+        private System.Windows.Forms.TextBox textBoxYazar;
+        private System.Windows.Forms.TextBox textBoxISBN;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button ekle;
+        private System.Windows.Forms.Button sil;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ LibraryProject/LibraryProject/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LibraryProject
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter DA;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Kitap()
+        {
+            //baglanti = new SqlConnection("server=.;Initial Catalog=LİBRARYMANAGEMENT;Integrated Security=SSPI");
+            baglanti = new SqlConnection("Data Source = DESKTOP-LGMC8P4\\SQLEXPRESS ;Initial Catalog=LİBRARYMANAGEMENT;Integrated Security=SSPI");
+            baglanti.Open();
+            DA = new SqlDataAdapter("SELECT *FROM Book_Table" , baglanti);
+            DataTable Tablo = new DataTable();
+            DA.Fill(Tablo);
+            dataGridView1.DataSource = Tablo;
+            baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Kitap();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBoxAd.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBoxYazar.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBoxISBN.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            
+        }
+
+        private void ekle_Click(object sender, EventArgs e)
+        {
+            string Sorgu = "INSERT INTO Book_Table(Title,Author,ISBN) VALUES (@Title,@Author,@ISBN)";
+            komut = new SqlCommand(Sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Title", textBoxAd.Text);
+            komut.Parameters.AddWithValue("@Author", textBoxYazar.Text);
+            komut.Parameters.AddWithValue("@ISBN", textBoxISBN.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            Kitap();
+        }
+
+        private void sil_Click(object sender, EventArgs e)
+        {
+            string Sorgu = "DELETE FROM Book_Table WHERE Title=@Title";
+            komut = new SqlCommand(Sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Title", textBoxAd.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            Kitap();
+        }
+    }
+}
